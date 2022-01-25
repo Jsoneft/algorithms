20ca6a80f285d20309be0b0c1c01f1d3a606e9ce
Jsoneft <jasonleft@qq.com>
2022-01-26 00:49:42 +0800

    feat: update atcoder 1.abc236_a

---
+++ atcoder/abc236_a.go
@@ -0,0 +1,33 @@
+package atcoder
+
+// Problem description URL https://atcoder.jp/contests/abc236/tasks/abc236_a
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	in  = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+)
+
+func scan(a ...interface{})             { fmt.Fscan(in, a...) }
+func printf(f string, a ...interface{}) { fmt.Fprintf(out, f, a...) }
+func solve() {
+	str := ""
+	scan(&str)
+
+	l, r := 0, 0
+	scan(&l, &r)
+	sBytes := []byte(str)
+	sBytes[l-1], sBytes[r-1] = sBytes[r-1], sBytes[l-1]
+	//printf("str: %s, l = %c, r = %c",sBytes, sBytes[l-1], sBytes[r-1])
+	printf(string(sBytes))
+
+}
+func main() {
+	defer out.Flush()
+	solve()
+}
